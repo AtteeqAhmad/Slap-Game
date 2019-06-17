433dae1dc2584af23835231c34e5671ad03c90a5
Your Name <Your email>
2019-06-17 10:05:21 -0600

    ..

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
+let extras = {
+      clap: {
+            name: 'clap',
+            modifier: 5,
+            description: 'faceinclap',
+      },
+      munch: {
+            name: 'munch',
+            modifier: 7,
+            description: 'dblpunch',
+      },
+      klick: {
+            name: 'klick',
+            modifier: 9,
+            description: 'dblkick',
+      },
+}
 let health = 100
 
 
-let powerElem = document.querySelector('#power')
+      let hitElem = document.querySelector('#hit')
+      let powerElem = document.querySelector('#power')
+      let healthElem = document.querySelector('#health')
+      let targetElem = document.querySelector('#target')
 let resetElem = document.querySelector('#reset')
-let healthElem = document.querySelector('#health')
-let imgElem = document.querySelector('#img')
 
-// power up to certain level
+
+function extra(extra){
+      health.modifier += extra[extra].modifier;
+}
+      // power up to certain level
 function power(){
-      health += 11
+health += 11
       drawhealth()
       console.log("power")
 }
 
+function hit(){
+      hit += 1
+      drawhit()
+
+}
+
 // decreasing power by slapping and func
 function slap(){
-      health -= 3
+      health -= 1
       drawhealth()
       console.log("slapworks")
 
@@ -23,30 +51,37 @@ function slap(){
 // decrease health by punching
 function punch() {
       health -= 2
+      health.hit++
       drawhealth()
       console.log("punchworks")
 }
 // decrease health by kicking
 function kick() {
       health -= 3
+      hit++
       drawhealth()
       console.log("kickworks")
 }
 
+
+
 function clap() {
       health -= 5
+      hit++
       drawhealth()
       console.log("clapworks")
 }
 
 function munch() {
       health -= 7
+      hit++
       drawhealth()
       console.log("munchworks")
 }
 
 function klick() {
       health -= 9
+      hit++
       drawhealth()
       console.log("klickworks")
 }
@@ -58,5 +93,5 @@ function drawhealth() {
 function reset(){
       health = 100
       drawhealth()
-      console.log("resetworks")
+      console.log("reset")
 }
