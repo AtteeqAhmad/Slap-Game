122370752c50a085f08bc5a520220b13c25fde47
Your Name <Your email>
2019-06-20 13:56:33 -0600

    .

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 
 let health = 100
 
-function additem(item) {
-      target.modifier += items[item].modifier;
-      fighter.modifier += items[item].modifier;
+function additem(itemName) {
+      target.modifier += items[itemName].modifier;
+      console.log(target.modifier)
+      fighter.modifier += items[itemName].modifier;
 }
-      let hitElem = document.querySelector('#hit')
-      let powerElem = document.querySelector('#power')
-      let healthElem = document.querySelector('#health')
-      let targetElem = document.querySelector('#target')
-      let resetElem = document.querySelector('#reset')
+let hitElem = document.querySelector('#hit')
+let powerElem = document.querySelector('#power')
+let healthElem = document.querySelector('#health')
+let targetElem = document.querySelector('#target')
+let resetElem = document.querySelector('#reset')
 
 let target = {
       health: 100,
+      modifier: 0,
       attacks: {
             slap: 1,
             punch: 3,
@@ -23,68 +25,71 @@ let target = {
       }
 }
 
- let fighter = {
-            health: 100,
-            attacks: {
-                  slap: 2,
-                  punch: 2.5,
-                  kick: 5.5,
-                  clap: 3.5,
-                  munch: 7.5,
-                  klick: 9.5
-            }
+let fighter = {
+      health: 100,
+      modifier: 0,
+      attacks: {
+            slap: 2,
+            punch: 2.5,
+            kick: 5.5,
+            clap: 3.5,
+            munch: 7.5,
+            klick: 9.5
+      }
 }
-      // power up to certain level
-function power(){
-health += 11
+// power up to certain level
+function power() {
+      health += 11
       drawhealth()
       console.log("power")
 }
 
-function hit(){
+function hit() {
       hit += 1
       drawhit()
 
 }
 
 // decreasing power by slapping and func
-function slap(){
-      health -= 1
+function slap() {
+      health -= 1 + target.modifier
       drawhealth()
       console.log("slapworks")
 
 }
 // decrease health by punching
 function punch() {
-      health -= 2
+      health -= 2 + target.modifier
       health.hit++
       drawhealth()
       console.log("punchworks")
 }
 // decrease health by kicking
 function kick() {
-      health -= 3
+      health -= 3 + target.modifier
       hit++
       drawhealth()
       console.log("kickworks")
 }
 
 
-let item = [clap = {
-      name: 'Clap',
-      modifier: 5,
-description: 'dblslap'
-},
-munch = {
-      name: 'Munch',
-      modifier: 7,
-      description: 'dblpunch'
-},
- klick = {
-      name: 'Klick',
-      modifier: 9,
-      description: 'dblkick'
-}]
+let items = {
+      clap: {
+            name: 'Clap',
+            modifier: 5,
+            description: 'dblslap'
+      },
+      munch: {
+            name: 'Munch',
+            modifier: 7,
+            description: 'dblpunch'
+      },
+      klick: {
+            name: 'Klick',
+            modifier: 9,
+            description: 'dblkick'
+      }
+}
 
 function drawhit() {
       hitElem.textContent = hit
@@ -94,7 +99,7 @@ function drawhealth() {
       healthElem.textContent = health
 }
 
-function reset(){
+function reset() {
       health = 100
       drawhealth()
       console.log("reset")
